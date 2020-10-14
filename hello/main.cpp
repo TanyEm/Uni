e88273e429698989bcd13a1a36c9ebcab33afc0a
TanyEm <tanyem@protonmail.com>
2020-10-15 01:10:32 +0300

    init commit

---
+++ hello/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Oрганизация процессов и программирование в среде Linux\n";
+    cout << "Группа: 7005\n";
+    cout << "Подлесных Т.П\n";
+    return 0;
+}
